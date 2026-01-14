2ad616e36a2bd3584826b2b1965905bfeef3deb2
Son Nguyen <son.nguyen@codeleap.de>
2026-01-14 11:07:14 +0700

    feat: set up page paths, initialize main page

---
+++ src/app/pages/main-page/main-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [],
+  template: `
+    <p>
+      main-page works!
+    </p>
+  `,
+  styles: ``,
+})
+class MainPage{}
+export default MainPage;
