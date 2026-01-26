a086c6b180b8e1d394e5d251339d78584a42271c
Son Nguyen <son.nguyen@codeleap.de>
2026-01-26 15:21:19 +0700

    refactor: use interceptor for api response errors instead of pipeable operators

--- src/app/core/api/breed.api.ts
+++ src/app/core/api/breed.api.ts
@@ -1,10 +1,10 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, throwError, map } from 'rxjs';
 import { ApiBreedImage, FavoriteBreedResponse } from '@type/breed';
 import { mapApiToBreed } from '@shared/utils/api.util';
 import { BASE_API_URL } from './api.token';
 import { environment } from '@environments/environment.development';
+
 @Injectable()
 export class BreedApiService {
   private readonly baseUrl = inject(BASE_API_URL);
@@ -16,25 +16,12 @@ export class BreedApiService {
       limit: limit.toString(),
       page: page.toString(),
     }).toString();
-    return this.httpClient.get<ApiBreedImage[]>(apiUrl.toString()).pipe(
-      catchError((error) => {
-        console.error('Error fetching breed info:', error);
-        return throwError(() => new Error('Failed to fetch breed info'));
-      }),
-    );
+    return this.httpClient.get<ApiBreedImage[]>(apiUrl.toString());
   }
 
   fetchBreedDetail(id: string) {
     const apiUrl = new URL(`/v1/images/${id}`, this.baseUrl);
-    return this.httpClient.get<ApiBreedImage>(apiUrl.toString()).pipe(
-      map((breedData) => {
-        return mapApiToBreed(breedData);
-      }),
-      catchError((error) => {
-        console.error('Error fetching breed info:', error);
-        return throwError(() => new Error('Failed to fetch breed info'));
-      }),
-    );
+    return this.httpClient.get<ApiBreedImage>(apiUrl.toString()).pipe(mapApiToBreed);
   }
 
   addToFavorites(breedId: string) {
@@ -43,12 +30,7 @@ export class BreedApiService {
       image_id: breedId,
       sub_id: environment.USER_ID,
     };
-    return this.httpClient.post(apiUrl.toString(), body).pipe(
-      catchError((error) => {
-        console.error('Error adding breed to favorites:', error);
-        return throwError(() => new Error('Failed to add breed to favorites'));
-      }),
-    );
+    return this.httpClient.post(apiUrl.toString(), body);
   }
 
   getFavorites() {
@@ -56,21 +38,11 @@ export class BreedApiService {
     apiUrl.search = new URLSearchParams({
       sub_id: environment.USER_ID,
     }).toString();
-    return this.httpClient.get<FavoriteBreedResponse[]>(apiUrl.toString()).pipe(
-      catchError((error) => {
-        console.error('Error fetching favorite breeds:', error);
-        return throwError(() => new Error('Failed to fetch favorite breeds'));
-      }),
-    );
+    return this.httpClient.get<FavoriteBreedResponse[]>(apiUrl.toString());
   }
 
   removeFromFavorites(favoriteId: number) {
     const apiUrl = new URL(`/v1/favourites/${favoriteId}`, this.baseUrl);
-    return this.httpClient.delete(apiUrl.toString()).pipe(
-      catchError((error) => {
-        console.error('Error removing breed from favorites:', error);
-        return throwError(() => new Error('Failed to remove breed from favorites'));
-      }),
-    );
+    return this.httpClient.delete(apiUrl.toString());
   }
 }
